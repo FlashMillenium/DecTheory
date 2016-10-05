7331cf9fc1404a3c75b3cac9529efbe7698f01f5
Daniil Kudryavtsev <kudryavtsev.d.e@gmail.com>
2016-10-05 13:00:56 +0300

    A bit fix of code. \n\nClearup and other stuff

--- Shl/src/main/java/ru/spbstu/dtheory/task1/Main.java
+++ Shl/src/main/java/ru/spbstu/dtheory/task1/Main.java
@@ -1,91 +1,109 @@
 package ru.spbstu.dtheory.task1;
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
+import static java.lang.Math.abs;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     public static void main(String[] args) {
+        mainCriteriaMethod();
+        generalCriteriaMethod();
+    }
 
-        ArrayList<List<Double>> mainCriteriaTable = new ArrayList<>();
-        mainCriteriaTable.add(Arrays.asList(0.008,  0.100,   0.5, 44000d, 500d, 2800000d, 0.3, 30d));
-        mainCriteriaTable.add(Arrays.asList( 0.01, 0.0625, 0.125, 70000d, 700d, 3000000d, 0.8, 45d));
-        mainCriteriaTable.add(Arrays.asList( 0.01, 0.1111,   0.2, 47000d, 500d, 2550000d, 0.2, 19d));
-        mainCriteriaTable.add(Arrays.asList( 0.01,  0.125,  0.05, 49000d, 600d, 2600000d, 0.6, 20d));
-        mainCriteriaTable.add(Arrays.asList(0.008,    0.2, 0.143, 45000d, 400d, 2500000d, 0.3, 13d));
-        mainCriteriaTable.add(Arrays.asList(0.003,   0.25, 0.167, 80000d, 600d, 3300000d, 0.1, 92d));
-        mainCriteriaTable.add(Arrays.asList(0.001,   0.75, 0.038, 85000d, 600d, 2500000d, 0.9, 11d));
+    private static void mainCriteriaMethod() {
+        Map<String, List<Double>> mainCriteriaTable = new HashMap<>();
+        mainCriteriaTable.put("Смена", asList(0.008, 0.100, 0.5, 44000d, 500d, 2800000d, 0.3, 30d));
+        mainCriteriaTable.put("Час Пик", asList(0.01, 0.0625, 0.125, 70000d, 700d, 3000000d, 0.8, 45d));
+        mainCriteriaTable.put("Невское Время", asList(0.01, 0.1111, 0.2, 47000d, 500d, 2550000d, 0.2, 19d));
+        mainCriteriaTable.put("Вечерний Петербург", asList(0.01, 0.125, 0.05, 49000d, 600d, 2600000d, 0.6, 20d));
+        mainCriteriaTable.put("СПб ведомости", asList(0.008, 0.2, 0.143, 45000d, 400d, 2500000d, 0.3, 13d));
+        mainCriteriaTable.put("Деловой Петербург", asList(0.003, 0.25, 0.167, 80000d, 600d, 3300000d, 0.1, 92d));
+        mainCriteriaTable.put("Реклама - Шанс", asList(0.001, 0.75, 0.038, 85000d, 600d, 2500000d, 0.9, 11d));
 
-        String[] nameCriteria = {"Смена" , "Час Пик", "Невское время", "Вечерний Петербург",
-                "СПб ведомости", "Деловой Петербург", "Реклама - Шанс"};
-
-        List<Double> restrictionList = Arrays.asList(0.01, 0.1, 0.038, 44000d, 400d, 2500000d, 0.3, 10d);
+        List<Double> restrictionList = asList(0.01, 0.1, 0.038, 44000d, 400d, 2500000d, 0.3, 10d);
 
         System.out.println("Table for Main Criteria");
-        mainCriteriaTable.forEach(System.out::println);
+        mainCriteriaTable.forEach((s, doubles) -> {
+            System.out.print(s);
+            System.out.println(doubles);
+        });
 
-        List<List<Double>> matchingList = mainCriteriaTable.parallelStream().filter(s ->
-                IntStream.range(0,s.size()).filter(i -> s.get(i) < restrictionList.get(i)).count() == 0)
-                .collect(Collectors.toList());
-
-        int index = IntStream.range(0, mainCriteriaTable.size()).filter(i -> mainCriteriaTable.get(i)
-                .equals(matchingList.get(0))).findFirst().getAsInt();
+        Map<String, List<Double>> matchingMap = mainCriteriaTable.entrySet()
+                .stream()
+                .filter(s -> IntStream.range(0, s.getValue().size())
+                        .allMatch(i -> s.getValue().get(i) >= restrictionList.get(i)))
+                // possible sorting here
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         System.out.println("\nMatching lists for Main Criteria:");
-//        matchingList.forEach(System.out::println); // win tuple
-//        System.out.println("Index is " + index);  // index win tuple and namecriteria
-//        System.out.println("Best newspaper for advertising: " + nameCriteria[index]);
-        System.out.println("| " + index + " | " + nameCriteria[index] + " | " + matchingList.get(0) + " |");
+        matchingMap.forEach((name, criteries) -> {
+            System.out.print(name);
+            System.out.println(criteries);
+        });
+    }
 
-
-        ArrayList<List<Double>> generalCriteriaTable = new ArrayList<>();
-        generalCriteriaTable.add(Arrays.asList(10d, 2d,  3d, 2d, 2d, 4d));
-        generalCriteriaTable.add(Arrays.asList( 4d, 7d,  3d, 4d, 3d, 1d));
-        generalCriteriaTable.add(Arrays.asList( 6d, 8d,  3d, 5d, 4d, 2d));
-        generalCriteriaTable.add(Arrays.asList( 9d, 2d,  3d, 2d, 1d, 3d));
-        generalCriteriaTable.add(Arrays.asList( 5d, 1d, 0.1, 5d, 2d, 2d));
-        generalCriteriaTable.add(Arrays.asList( 3d, 8d, 0.1, 2d, 4d, 1d));
-        generalCriteriaTable.add(Arrays.asList( 3d, 5d,  4d, 2d, 7d, 7d));
-        generalCriteriaTable.add(Arrays.asList( 3d, 6d,  3d, 4d, 4d, 1d));
-        generalCriteriaTable.add(Arrays.asList( 2d, 5d,  3d, 1d, 5d, 7d));
+    private static void generalCriteriaMethod() {
+        List<List<Double>> generalCriteriaTable = new ArrayList<>();
+        generalCriteriaTable.add(asList(10d, 2d, 3d, 2d, 2d, 4d));
+        generalCriteriaTable.add(asList(4d, 7d, 3d, 4d, 3d, 1d));
+        generalCriteriaTable.add(asList(6d, 8d, 3d, 5d, 4d, 2d));
+        generalCriteriaTable.add(asList(9d, 2d, 3d, 2d, 1d, 3d));
+        generalCriteriaTable.add(asList(5d, 1d, 0.1, 5d, 2d, 2d));
+        generalCriteriaTable.add(asList(3d, 8d, 0.1, 2d, 4d, 1d));
+        generalCriteriaTable.add(asList(3d, 5d, 4d, 2d, 7d, 7d));
+        generalCriteriaTable.add(asList(3d, 6d, 3d, 4d, 4d, 1d));
+        generalCriteriaTable.add(asList(2d, 5d, 3d, 1d, 5d, 7d));
 
 
         System.out.println("\nTable for General Criteria: ");
         generalCriteriaTable.forEach(System.out::println);
 
-        List<Double> list =  DoubleStream.iterate(0.1, n -> n +0.1).limit(9).boxed().collect(Collectors.toList());
+        List<Double> list = DoubleStream.iterate(0.1, n -> n + 0.1)
+                .limit(9)
+                .boxed()
+                .collect(toList());
 
-        List<HashMap<List<Double>, List<List<Double>>>> listalpha = Collections.synchronizedList(new ArrayList<>());
-        // List<HashMap<List<Double>, List<List<Double>>>> listalpha =
-        list.stream().flatMap(e1 -> list.stream()
-                .flatMap(e2 -> list.stream()
-                        .flatMap(e3 -> list.stream().
-                                flatMap(e4 ->  list.stream()
-                                        .map(e5 -> list.stream()
-                                                .filter(e6 -> Math.abs(1d - (e6+e5+e4+e3+e2+e1)) < 0.00000001)
-                                                .map(e6 -> Arrays.asList(e1, e2, e3, e4, e5, e6))
-                                                .collect(Collectors.toList())
-                                         )
+        double delta = 0.00000001;
+
+        List<HashMap<List<Double>, List<List<Double>>>> listalpha = list.stream()
+                .flatMap(e1 -> list.stream()
+                        .flatMap(e2 -> list.stream()
+                                .flatMap(e3 -> list.stream()
+                                        .flatMap(e4 -> list.stream()
+                                                .map(e5 -> list.stream()
+                                                        .filter(e6 -> abs(1d - (e6 + e5 + e4 + e3 + e2 + e1)) < delta)
+                                                        .map(e6 -> asList(e1, e2, e3, e4, e5, e6))
+                                                        .collect(toList())
+                                                )
+                                        )
                                 )
                         )
                 )
-                )
                 .filter(e -> !e.isEmpty())
-                .map(e -> makeAlphaMap(generalCriteriaTable,e.get(0)))
+                .map(e -> makeAlphaMap(generalCriteriaTable, e.get(0)))
                 .filter(e -> checkListEquals(e.values().stream().findFirst().get()))
-                .forEach(listalpha::add);
+                .collect(toList());
 
         HashMap<List<Double>, List<Double>> altHM = new HashMap<>();
-        listalpha.forEach(e -> altHM.put(e.values().stream().findFirst().get().get(0)
-                ,e.keySet().stream().findFirst().get()));
+        listalpha.forEach(e -> altHM.put(
+                e.values().stream().findFirst().get().get(0),
+                e.keySet().stream().findFirst().get())
+        );
 
 
         HashMap<List<Double>, List<List<Double>>> altHMSecond = new HashMap<>();
-        for(HashMap<List<Double>, List<List<Double>>> e: listalpha) {
-            if(!altHMSecond.containsKey(e.values().stream().findFirst().get().get(0))) {
+        for (HashMap<List<Double>, List<List<Double>>> e : listalpha) {
+            if (!altHMSecond.containsKey(e.values().stream().findFirst().get().get(0))) {
                 altHMSecond.put(e.values().stream().findFirst().get().get(0)
                         , new ArrayList<>());
             }
@@ -93,29 +111,25 @@ public class Main {
 
         }
 
-
-        System.out.println("\nAlpha and win tuple:");
-//        listalpha.forEach(e -> System.out.println(e.keySet().stream().findFirst().get() + " =" +
-//                " " + e.values().stream().findFirst().get().get(0))); // alpha -> win tuple
-//        altHM.entrySet().forEach(System.out::println);  // one win tuple -> one alpha
-//        System.out.println(listalpha.get(0 /* can be random number between 0 and listalpha.size*/)); // simple random output
-          altHMSecond.entrySet().forEach(System.out::println); // one win tuple -> several alpha
+        System.out.println("\nAlpha and win tuples:");
+        altHMSecond.entrySet().forEach(System.out::println); // one win tuple -> several alpha
     }
 
 
-    private static boolean checkListEquals(List<List<Double>> list){
+    private static boolean checkListEquals(List<List<Double>> list) {
         List<Double> value = list.get(0);
-        return list.stream().filter(e-> value.equals(e)).count() == list.size();
+        return list.stream().allMatch(value::equals);
     }
 
-    private static HashMap<List<Double> , List<List<Double>>> makeAlphaMap
-            (ArrayList<List<Double>> generalCriteriaTable, List<Double> alphaList){
-        HashMap<List<Double> , List<List<Double>>> result = new HashMap<>();
+    private static HashMap<List<Double>, List<List<Double>>> makeAlphaMap(
+            List<List<Double>> generalCriteriaTable, List<Double> alphaList
+    ) {
+        HashMap<List<Double>, List<List<Double>>> result = new HashMap<>();
 
         HashMap<Double, List<Double>> addFolging = additiveFolging(generalCriteriaTable, alphaList);
-        HashMap<Double, List<Double>> mulFolging = multiFolging(generalCriteriaTable,alphaList);
-        HashMap<Double, List<Double>> mFolging = minFolging(generalCriteriaTable,alphaList);
-        HashMap<Double, List<Double>> mxFolging = maxFolging(generalCriteriaTable,alphaList);
+        HashMap<Double, List<Double>> mulFolging = multiFolging(generalCriteriaTable, alphaList);
+        HashMap<Double, List<Double>> mFolging = minFolging(generalCriteriaTable, alphaList);
+        HashMap<Double, List<Double>> mxFolging = maxFolging(generalCriteriaTable, alphaList);
 
         List<List<Double>> entry = new ArrayList<>();
 
@@ -128,98 +142,100 @@ public class Main {
 
         return result;
     }
-    private static HashMap<Double, List<Double>> additiveFolging
-            (ArrayList<List<Double>> generalCriteriaTable, List<Double> alphaList){
 
-        return generalCriteriaTable.stream().map(e -> makeAddMap(e,alphaList))
-                .reduce((i,j) -> i.keySet().stream().findFirst().get() >
-                        j.keySet().stream().findFirst().get() ? i : j )
+    private static HashMap<Double, List<Double>> additiveFolging(
+            List<List<Double>> generalCriteriaTable, List<Double> alphaList
+    ) {
+        return generalCriteriaTable.stream().map(e -> makeAddMap(e, alphaList))
+                .reduce((i, j) -> i.keySet().stream().findFirst().get() >
+                        j.keySet().stream().findFirst().get() ? i : j)
                 .get();
 
     }
 
-    private static HashMap<Double, List<Double>> makeAddMap
-            (List<Double> genCriteriaTuple,List<Double> alphaList){
-
+    private static HashMap<Double, List<Double>> makeAddMap(
+            List<Double> genCriteriaTuple, List<Double> alphaList
+    ) {
         HashMap<Double, List<Double>> result = new HashMap<>();
 
-        result.put(IntStream.range(0,genCriteriaTuple.size()).
-                        mapToObj(i -> genCriteriaTuple.get(i)*alphaList.get(i))
-                        .reduce((i,j) -> i+j)
-                        .get()
-                ,genCriteriaTuple);
+        result.put(IntStream.range(0, genCriteriaTuple.size()).
+                        mapToObj(i -> genCriteriaTuple.get(i) * alphaList.get(i))
+                        .reduce((i, j) -> i + j)
+                        .get(),
+                genCriteriaTuple);
 
         return result;
     }
 
-    private static HashMap<Double, List<Double>> maxFolging
-            (ArrayList<List<Double>> generalCriteriaTable, List<Double> alphaList){
-
-        return generalCriteriaTable.stream().map(e -> makeMaxMap(e,alphaList))
-                .reduce((i,j) -> i.keySet().stream().findFirst().get() >
+    private static HashMap<Double, List<Double>> maxFolging(
+            List<List<Double>> generalCriteriaTable, List<Double> alphaList
+    ) {
+        return generalCriteriaTable.stream().map(e -> makeMaxMap(e, alphaList))
+                .reduce((i, j) -> i.keySet().stream().findFirst().get() >
                         j.keySet().stream().findFirst().get() ? i : j)
                 .get();
     }
 
     private static HashMap<Double, List<Double>> makeMaxMap
-            (List<Double> genCriteriaTuple,List<Double> alphaList) {
+            (List<Double> genCriteriaTuple, List<Double> alphaList) {
 
         HashMap<Double, List<Double>> result = new HashMap<>();
 
-        result.put(IntStream.range(0,genCriteriaTuple.size())
-                        .mapToObj(i -> genCriteriaTuple.get(i)*alphaList.get(i))
+        result.put(IntStream.range(0, genCriteriaTuple.size())
+                        .mapToObj(i -> genCriteriaTuple.get(i) * alphaList.get(i))
                         .max(Double::compareTo)
                         .get()
-                ,genCriteriaTuple);
+                , genCriteriaTuple);
 
         return result;
     }
 
     private static HashMap<Double, List<Double>> minFolging
-            (ArrayList<List<Double>> generalCriteriaTable, List<Double> alphaList){
+            (List<List<Double>> generalCriteriaTable, List<Double> alphaList) {
 
-        return generalCriteriaTable.stream().map(e -> makeMinMap(e,alphaList))
-                .reduce((i,j)->i.keySet().stream().findFirst().get() >
+        return generalCriteriaTable.stream().map(e -> makeMinMap(e, alphaList))
+                .reduce((i, j) -> i.keySet().stream().findFirst().get() >
                         j.keySet().stream().findFirst().get() ? i : j)
                 .get();
     }
+
     private static HashMap<Double, List<Double>> makeMinMap
-            (List<Double> genCriteriaTuple,List<Double> alphaList) {
+            (List<Double> genCriteriaTuple, List<Double> alphaList) {
 
         HashMap<Double, List<Double>> result = new HashMap<>();
 
-        result.put(IntStream.range(0,genCriteriaTuple.size()).
-                        mapToObj(i -> genCriteriaTuple.get(i)/alphaList.get(i))
+        result.put(IntStream.range(0, genCriteriaTuple.size()).
+                        mapToObj(i -> genCriteriaTuple.get(i) / alphaList.get(i))
                         .min(Double::compareTo)
                         .get()
-                ,genCriteriaTuple);
+                , genCriteriaTuple);
 
         return result;
     }
 
 
     private static HashMap<Double, List<Double>> multiFolging
-            (ArrayList<List<Double>> generalCriteriaTable, List<Double> alphaList){
+            (List<List<Double>> generalCriteriaTable, List<Double> alphaList) {
 
         return generalCriteriaTable.stream().map(e -> makeMultMap(e, alphaList))
-                .reduce((i,j)->i.keySet().stream().findFirst().get() >
+                .reduce((i, j) -> i.keySet().stream().findFirst().get() >
                         j.keySet().stream().findFirst().get() ? i : j)
                 .get();
     }
+
     private static HashMap<Double, List<Double>> makeMultMap
-            (List<Double> genCriteriaTuple,List<Double> alphaList){
+            (List<Double> genCriteriaTuple, List<Double> alphaList) {
 
         HashMap<Double, List<Double>> result = new HashMap<>();
 
-        result.put(IntStream.range(0,genCriteriaTuple.size())
-                        .mapToObj(i -> Math.pow(genCriteriaTuple.get(i),alphaList.get(i)))
-                        .reduce((i,j) -> i*j)
+        result.put(IntStream.range(0, genCriteriaTuple.size())
+                        .mapToObj(i -> Math.pow(genCriteriaTuple.get(i), alphaList.get(i)))
+                        .reduce((i, j) -> i * j)
                         .get()
-                ,genCriteriaTuple);
+                , genCriteriaTuple);
 
         return result;
     }
-
 
 
 }
